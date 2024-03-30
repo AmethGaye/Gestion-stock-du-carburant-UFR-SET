efb9cebd5ecd73c8a6ba0ab4c1d04f8c52e8af19
BaMamadou12 <mamadou.ba2@univ-thies.sn>
2024-03-30 15:02:56 +0000

    modification de la table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->date('date_naiss');
+            $table->string('telephone')->nullable()->unique();
+            $table->string('role');
+            $table->string('status');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
