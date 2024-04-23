0978129425957418f2a38aacdace5ca98eb7e35e
BaMamadou12 <mamadou.ba2@univ-thies.sn>
2024-04-23 23:02:21 +0000

    problem resolu

--- resources/views/partials/nav-bar.blade.php
+++ resources/views/partials/nav-bar.blade.php
@@ -7,7 +7,7 @@
                     <path d="M15.178 16.0683L10.4655 11.0455M12.052 6.87446C12.0433 10.1027 9.56776 12.7132 6.52264 12.7052C3.47752 12.6971 1.01599 10.0736 1.02465 6.84531C1.03332 3.61704 3.5089 1.00652 6.55402 1.01457C9.59914 1.02262 12.0607 3.64618 12.052 6.87446Z" stroke="#71717A" stroke-width="1.56321" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </label>
-            <input type="text" name='search' id="main-search" value="{{$request()->search}}" placeholder="Rechercher" class="pr-3 pl-9 py-1.5 outline-none w-80 bg-transparent text-sm text-zinc-500 border border-zinc-300 bg-zinc-100 rounded focus:border-zinc-500">
+            <input type="text" name='search' id="main-search" value="" placeholder="Rechercher" class="pr-3 pl-9 py-1.5 outline-none w-80 bg-transparent text-sm text-zinc-500 border border-zinc-300 bg-zinc-100 rounded focus:border-zinc-500">
         </div>
     </form>
     <div class="flex items-center gap-4 ">
